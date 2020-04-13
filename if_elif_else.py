0b1864b808401bf10e9ca38307fa014ed6627768
rajendra07negi <rajendra07negi@gmail.com>
2020-04-13 12:22:06 +0530

    first

---
+++ if_elif_else.py
@@ -0,0 +1,7 @@
+x = 2
+if x == 2:
+    print('x is 2.')
+elif x >= 2:
+    print('x is greater than 2 ', x)
+else:
+    print('x is less than 2')
---
+++ while.py
@@ -0,0 +1,9 @@
+x = 1
+while x <= 5:
+    print("hello ", end="")
+    y = 1
+    while y <= 5:
+        print("World! ",end="")
+        y = y+1
+    x = x + 1
+    print()
